d968879084c6fa478b8263f38a3ef2d1c0c1c5d9
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-12-11 08:03:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,8 @@
 #include "lib.h"
-float numerOmassimo (const float numeri[], int dim){
+float numeroMassimo (const float numeri[], int dim){
   numeri [dim] ;
-  float massimo = numeri [0];
-  for( int i=0 ; i<dim ; i++){
+  float massimo = numeri[0];
+  for ( int i=0 ; i < dim ; i++){
     if(massimo < numeri[i]){
       massimo = numeri [i];
     }
